d1a5823a0c2aaae29e78ce4a0b396e7e11d35bd0
Mykhailo-ai <84092719+Mykhailo-ai@users.noreply.github.com>
2022-02-04 15:57:04 +0200

    TechTree
    TechTree

---
+++ TechTreeMVCWebApplication/TechTreeMVCWebApplication/Data/Migrations/20211116093113_AddAdminAccount.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Text;
+
+namespace TechTreeMVCWebApplication.Data.Migrations
+{
+    public partial class AddAdminAccount : Migration
+    {
+        const string ADMIN_USER_GUID = "5ac562de-8e69-4cba-86a6-a1eb7ce3f498";
+        const string ADMIN_ROLE_GUID = "b2237eea-a1e9-468a-a498-cc71fa4fdbf7";
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var hasher = new PasswordHasher<ApplicationUser>();
+            var passwordHash = hasher.HashPassword(null, "Password100!");
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("INSERT INTO AspNetUsers (Id, UserName, NormalizedUserName, Email, EmailConfirmed, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnabled, AccessFailedCount, NormalizedEmail, PasswordHash, SecurityStamp, FirstName)");
+            sb.AppendLine("VALUES(");
+            sb.AppendLine($"'{ADMIN_USER_GUID}'");
+            sb.AppendLine(", 'admin@techtree.co.uk'");
+            sb.AppendLine(", 'ADMIN@TECHTREE.CO.UK'");
+            sb.AppendLine(", 'admin@techtree.co.uk'");
+            sb.AppendLine(", '0'");
+            sb.AppendLine(", '0'");
+            sb.AppendLine(", '0'");
+            sb.AppendLine(", '0'");
+            sb.AppendLine(", '0'");
+            sb.AppendLine(", 'ADMIN@TECHTREE.CO.UK'");
+            sb.AppendLine($", '{passwordHash}'");
+            sb.AppendLine(", ''");
+            sb.AppendLine(", 'admin'");
+            sb.AppendLine(")");
+
+            migrationBuilder.Sql(sb.ToString());
+            migrationBuilder.Sql($"INSERT INTO AspNetRoles (Id, Name, NormalizedName) VALUES ('{ADMIN_ROLE_GUID}', 'admin', 'ADMIN')");
+            migrationBuilder.Sql($"INSERT INTO AspNetUserRoles (UserId, RoleId) VALUES ('{ADMIN_USER_GUID}', '{ADMIN_ROLE_GUID}')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"DELETE FROM AspNetUserRoles WHERE UserId = '{ADMIN_USER_GUID}' AND RoleId = '{ADMIN_ROLE_GUID}'");
+            migrationBuilder.Sql($"DELETE FROM AspNetRoles WHERE Id = '{ADMIN_ROLE_GUID}'");
+            migrationBuilder.Sql($"DELETE FROM AspNetUsers WHERE Id = '{ADMIN_USER_GUID}'");
+        }
+    }
+}
---
+++ TechTreeMVCWebApplication/TechTreeMVCWebApplication/Entities/Content.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechTreeMVCWebApplication.Entities
+{
+    public class Content
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 2)]
+        public string Title { get; set; }
+
+        [Display(Name = "HTML Content")]
+        public string HTMLContent { get; set; }
+
+        [Display(Name = "Video Link")]
+        public string VideoLink { get; set; }
+
+        public CategoryItem CategoryItem { get; set; }
+
+        [NotMapped]
+        public int CatItemId { get; set; }
+        //Note: this property cannot be named CategoryItemId because this would
+        //interfere with future migrations
+        //It has been named like this so with not to conflict with EF Core
+
+        [NotMapped]
+        public int CategoryId { get; set; }
+    }
+}
